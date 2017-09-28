6d191137d2678d62683312038ac9aa79f34ecf5d
Veronica Perez <veronica.pmail@gmail.com>
2017-09-28 17:05:06 +0200

    Data binding

--- src/app/shared/dish.ts
+++ src/app/shared/dish.ts
@@ -1,8 +1,10 @@
-export class Dish {
+import { Comment } from './comment';
+    export class Dish {
     name: string;
     image: string;
     category: string;
     label: string;
     price: string;
     description: string;
+    comments: Comment[];
 }
